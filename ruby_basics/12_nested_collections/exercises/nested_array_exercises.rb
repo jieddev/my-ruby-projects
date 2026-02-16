d9a0022759cf6581a553ea9f2ea11e108071086d
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-16 18:08:43 +0800

    Continue to complete find_favorite method

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -1,4 +1,5 @@
 def blank_seating_chart(number_of_rows, seats_per_row)
+  Array.new(number_of_rows) { Array.new(seats_per_row) }
   # return a 2d array to represent a seating chart that contains
   # number_of_rows nested arrays, each with seats_per_row entries of nil to
   # represent that each seat is empty.
@@ -16,11 +17,15 @@ end
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
   # at row_index index of the chart, then return the chart
+  chart[row_index].push(seat_to_add)
+  return chart
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
   # then return the chart.
+  chart.push(row_to_add)
+  return chart
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
@@ -28,15 +33,29 @@ def delete_seat_from_row(chart, row_index, seat_index)
   # the chart, then return the chart
 
   # Hint: explore the ruby docs to find a method for deleting from an array!
+  chart[row_index].delete_at(seat_index)
+  return chart
 end
 
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
   # then return the chart
+  chart.delete_at(row_index)
+  return chart
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
+  total_of_nil = 0
+  
+  chart.each do |index|
+    index.each do |index2|
+      if index2 == nil
+        total_of_nil += 1  
+      end
+    end
+  end 
+  return total_of_nil
 
   # NOTE: `chart` should **not** be mutated
 end
@@ -55,4 +74,6 @@ def find_favorite(array_of_hash_objects)
 
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
+
+  
 end

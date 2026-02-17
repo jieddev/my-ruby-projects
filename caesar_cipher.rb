a131a044c8314b3005b253e07e816c35ae81308f
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-17 15:06:53 +0800

    Complete caesar cipher project using brute force method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,6 +1,34 @@
-def caesar_cipher(text, shift)
-  return text.to_i
+def caesar_cipher(string, shift)
+  splitted_string = string.split("")
+  small_alphabetical_letters = ('a'..'z').to_a
+  encrypted_string = ""
 
+  splitted_string.each do |letter|
+    if letter == " "
+      encrypted_string += " "
+    elsif letter == "!"
+      encrypted_string += "!"
+
+    elsif letter == letter.downcase()
+      index_of_letter = small_alphabetical_letters.index(letter)
+      shifted_letter = small_alphabetical_letters[index_of_letter + shift]
+      encrypted_string += shifted_letter
+    
+    elsif letter == letter.upcase()
+      letter_small = letter.downcase()
+      index_of_letter = small_alphabetical_letters.index(letter_small)
+      shifted_letter = (index_of_letter + shift ) % 26
+      shifted_letter_upcase = small_alphabetical_letters[shifted_letter].upcase()
+      encrypted_string += shifted_letter_upcase
+  
+    end
+  end
+
+  puts encrypted_string
+
+  # shifted_letter = ( index_of_letter + shift) % 26
 end
 
+
 caesar_cipher("What a string!", 5)
+

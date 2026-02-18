c4498bf952d2d0d5bfa2556b96ef4e01b1a3682d
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-18 14:57:18 +0800

    Refactor bubble_sort method by adding number_of-times_to_iterate variable to set the length and wrap each_with_index to a #times loop to iterate using the size of the length of the array

--- src/main.rb
+++ src/main.rb
@@ -1,25 +1,31 @@
 def bubble_sort(array_of_numbers)
   sorted_array = []
+  number_of_times_to_iterate = array_of_numbers.length
 
-  array_of_numbers.each_with_index do |number, index|
-    if index > 0
+  number_of_times_to_iterate.times do |i|
+    array_of_numbers.each_with_index do |number, index|
+      if index > 0
 
-      left_number = array_of_numbers[index - 1]
-      right_number = array_of_numbers[index]
+        left_number = array_of_numbers[index - 1]
+        right_number = array_of_numbers[index]
 
-      # puts left_number
-      # puts right_number
-      
-      if left_number > right_number
-        array_of_numbers[index - 1] = right_number
-        array_of_numbers[index] = left_number
+        # puts left_number
+        # puts right_number
+        
+        if left_number > right_number
+          array_of_numbers[index - 1] = right_number
+          array_of_numbers[index] = left_number
+        end
+
+        sorted_array = array_of_numbers
       end
-      
+    
+      # number_of_times_to_iterate -= 1  
     end
   end
 
-  return array_of_numbers
+  return sorted_array
 end
 
-# bubble_sort([4,3,78,2,0,2])
-p bubble_sort([3, 2, 1])
+p bubble_sort([4,3,78,2,0,2])
+# p bubble_sort([3, 2, 1])

7d85540571359a638b21daac0c34aed8bd118aeb
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-18 13:20:42 +0800

    Update main.rb to add another method called find_index_of_best_day_to_buy and find_index_of_best_day_to_sell to return the index of the day to buy and sell to yield the biggest profit

--- src/main.rb
+++ src/main.rb
@@ -2,6 +2,7 @@ def stock_picker(days)
   profit = 0
   profits = []
   days_passed = []
+  output = []
 
   days.each_with_index do |day, index|
     sell_day_index = days.index(day)
@@ -22,7 +23,15 @@ def stock_picker(days)
 
   best_day_to_buy_and_sell = find_best_day_to_buy_and_sell(biggest_profit, no_zero_profits)
 
-  return best_day_to_buy_and_sell
+  # return best_day_to_buy_and_sell
+
+  best_day_to_buy = find_index_of_best_day_to_buy(days, best_day_to_buy_and_sell)
+  best_day_to_sell = find_index_of_best_day_to_sell(days, best_day_to_buy_and_sell)
+
+  output << best_day_to_buy
+  output << best_day_to_sell
+  return output
+
 end
 
 def filter_profit_to_return_positive_non_zero_profit(profits) 
@@ -45,6 +54,16 @@ def find_best_day_to_buy_and_sell(biggest_profit, no_zero_profits)
   best_day_to_buy_and_sell.delete_at(2)
 
   return best_day_to_buy_and_sell
+end
+
+def find_index_of_best_day_to_buy(days, best_day_to_buy_and_sell) 
+  best_day_to_buy = days.index(best_day_to_buy_and_sell[0])
+  return best_day_to_buy
+end
+
+def find_index_of_best_day_to_sell(days, best_day_to_buy_and_sell)
+  best_day_to_sell = days.index(best_day_to_buy_and_sell[1])
+  return best_day_to_sell
 end
 
 

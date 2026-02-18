6551a32aeabdbe128e4cb3f46be6bd95976c574b
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-18 16:47:22 +0800

    Add my_classes folder and create atm_machine.rb file that has a deposit and withdraw method for users to type what transaction they want and return a colored message base on the transaction

---
+++ my_classes/atm_machine.rb
@@ -0,0 +1,45 @@
+class AtmMachine
+
+  def deposit(money_to_deposit)
+    puts "\e[32mYou've deposited ₱#{money_to_deposit}!\e[0m"
+  end
+
+  def withdraw(money_to_withdraw)
+    puts "\e[31mYou've withdrawn ₱#{money_to_withdraw}\e[0m"
+  end
+end
+
+
+def ask_amount()
+  print "Amount: "
+  user_inputted_amount = gets.chomp
+  return user_inputted_amount
+end
+
+
+def transact()
+  transaction_1 = AtmMachine.new
+
+  print "Jieddev ATM Machine (Deposit/Withdraw/Balance/Exit): "
+  user_input = gets.chomp
+
+  if user_input == "Exit"
+    puts "Thank you for using Jieddev ATM Machine!"
+    return
+  end
+
+  case user_input
+  when "Deposit"
+    amount = ask_amount()
+    transaction_1.deposit(amount)
+
+  when "Withdraw"
+    amount = ask_amount()
+    transaction_1.withdraw(amount)
+  end
+
+  transact()
+
+end
+
+transact()

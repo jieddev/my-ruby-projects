32729eff57385ee83aa038cf2cf9e1b045f3ca29
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-18 14:30:06 +0800

    Update bubble_sort method to swap the left number to the right number if the left number is greater than the right number

--- src/main.rb
+++ src/main.rb
@@ -2,15 +2,23 @@ def bubble_sort(array_of_numbers)
   sorted_array = []
 
   array_of_numbers.each_with_index do |number, index|
-    if index < array_of_numbers.length - 1
-      if number > array_of_numbers[index + 1]
-        sorted_array.push(array_of_numbers[index + 1])
-        sorted_array.push(number)
+    if index > 0
+
+      left_number = array_of_numbers[index - 1]
+      right_number = array_of_numbers[index]
+
+      # puts left_number
+      # puts right_number
+      
+      if left_number > right_number
+        array_of_numbers[index - 1] = right_number
+        array_of_numbers[index] = left_number
       end
+      
     end
   end
 
-  return sorted_array
+  return array_of_numbers
 end
 
 # bubble_sort([4,3,78,2,0,2])

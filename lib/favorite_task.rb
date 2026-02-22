fa11f3ba60a1fa08495a890810adc19defee96fc
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-22 10:48:11 +0800

    fix: Refactor FavoriteTask class to remove super.merge into to_hash methods to avoid coupling and change attr_accessor and initialize method in FavoriteTask Class

--- lib/favorite_task.rb
+++ lib/favorite_task.rb
@@ -1,19 +1,23 @@
 require_relative 'task'
 
 class FavoriteTask < Task
-  attr_accessor :priority, :favorite_date
+  attr_accessor :priority, :mark_as_favorite
 
-  def initialize(mark_as_favorite)
+  def initialize(task_id:, task_name:, start_date:, end_date:, priority:, mark_as_favorite:)
     super(task_id: task_id, task_name: task_name, start_date: start_date, end_date: end_date)
     @priority = priority
-    @favorite_date = favorite_date
+    @mark_as_favorite = mark_as_favorite
   end
 
   def to_hash
-    super.merge({
-      priority: @priority,
-      favorite_date: @favorite_date
-    })
+    return {
+      task_id: @task_id
+      task_name: @task_name
+      start_date: @start_date
+      end_date: @end_date
+      priority: @priority
+      mark_as_favorite: @mark_as_favorite
+    }
   end
 
   def high_priority?

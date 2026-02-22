0e076db12c190da2cfaaf4d060df95291d1c5c96
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-22 15:16:17 +0800

    Update main.rb to use functional programming as a bruteforce to create Mastermind Game and add code to print the color_code_to_balls after the times loop ends

--- main.rb
+++ main.rb
@@ -1,25 +1,70 @@
-def computer_create_code()
-  color_code = []
-
-  4.times do 
-    color_code.push(rand(1..6))
-    
-  end
-
-  return color_code
-
-end
-
-def convert_array_to_color_balls(color_code)
-  colors = {
+colors = {
     1 => "🔴",
     2 => "🔵",
     3 => "🟢",
     4 => "🟡",
     5 => "🟣",
-    6 => "🟠"
+    6 => "🟠", 
+    "1" => "🔴",
+    "2" => "🔵",
+    "3" => "🟢",
+    "4" => "🟡",
+    "5" => "🟣",
+    "6" => "🟠"
   }
 
+attempts = 12
+  
+def start_game(colors, attempts)
+  color_code = computer_create_code
+  puts "Here is the created color code: "
+  color_code_to_color_balls = convert_array_to_color_balls(color_code, colors)
+
+  attempts.times do
+    puts "Attempt ##{attempts}" 
+    print "Please enter your guess: "
+    guess = gets.chomp
+    attempts -= 1
+
+    guess_to_array = guess.split("")
+
+    guess_to_color_balls = convert_array_to_color_balls(guess_to_array, colors)
+    
+    puts "Your guess is: #{guess_to_color_balls}" 
+
+    puts " "
+
+    check_if_guess_matches_with_code(guess_to_color_balls, color_code_to_color_balls)
+
+  end
+
+  puts "The color code is: #{color_code_to_color_balls}"  
+
+
+
+end
+
+def check_if_guess_matches_with_code(guess_to_color_code, color_code_to_color_balls)
+  # p guess_to_color_code
+  
+  if guess_to_color_code == color_code_to_color_balls
+    puts " "
+    puts "That's Bingo! You Win! "
+  end
+
+end
+
+  
+def computer_create_code()
+  color_code = []
+  4.times do 
+    color_code.push(rand(1..6))
+    
+  end
+  return color_code
+end
+
+def convert_array_to_color_balls(color_code, colors)
   converted_color = []
   
   color_code.each do |color_number|
@@ -27,11 +72,14 @@ def convert_array_to_color_balls(color_code)
   end
 
   return converted_color
-
 end
 
-puts "--- Mastermind Minigame ---"
-puts ""
-color_code = computer_create_code
-p convert_array_to_color_balls(color_code)
-# p color_code
+def display_introduction
+  puts "--- Mastermind Minigame ---"
+  puts ""
+end
+
+
+
+start_game(colors, attempts)
+

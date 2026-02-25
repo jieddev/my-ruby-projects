cfc597f95983f381d7a1d60b049a98f61d15a9ca
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-25 20:43:31 +0800

    Refactor codebase to use OOP and add a Game Class in game.rb file and change code of main.rb to create an instance of Game class and call start_game instance method

--- lib/game.rb
+++ lib/game.rb
@@ -1,36 +1,90 @@
 class Game
-  COLORS = {
-    1 => "🔴",
-    2 => "🔵",
-    3 => "🟢",
-    4 => "🟡",
-    5 => "🟣",
-    6 => "🟠"
-  }
+  def initialize(colors, attempts)
+    @colors = colors
+    @attempts = attempts  
+  end
 
-  ATTEMPTS = 12
-  
-  def initialize()
+  def start_game()
+    @color_code = computer_create_code()
     
-  end
+    @color_code_to_color_balls = convert_array_to_color_balls(@color_code, @colors)
 
-  def start()
-    display_introduction
-  end
+    @attempts.times do
+      puts "Attempt ##{@attempts}" 
+      print "Please enter your guess: "
+      @guess = gets.chomp
+      @attempts -= 1
 
-  private
+      @guess_to_array = @guess.split("")
 
-  def display_introduction()
-    puts " === Mastermind Mini Game in Ruby by Jiedev ==="
-    puts ""
+      @guess_to_color_balls = convert_array_to_color_balls(@guess_to_array, @colors)
+      
+      puts "Your guess is: #{@guess_to_color_balls}" 
+
+      puts " "
+
+      @win = check_if_guess_matches_with_code(@guess_to_color_balls, @color_code_to_color_balls)
+
+      if @win == true 
+        break
+      end
+
+
+      @feedback = give_feedback_based_on_guess(@guess_to_color_balls, @color_code_to_color_balls)
+      puts "Feedback: #{@feedback}"
+
+    end
+
+    puts "The color code is: #{@color_code_to_color_balls}"  
+
   end
 
   def computer_create_code()
-    
-    
+    @color_code = []
+    4.times do 
+      @color_code.push(rand(1..6))
+      
+    end
+    return @color_code
   end
 
+  def convert_array_to_color_balls(color_code, colors)
+    @converted_color = []
+    
+    color_code.each do |color_number|
+      @converted_color.push(colors[color_number])
+    end
 
+    return @converted_color
+  end
+
+  def check_if_guess_matches_with_code(guess_to_color_code, color_code_to_color_balls)
+    
+    @win = true
+
+    if guess_to_color_code == color_code_to_color_balls
+      puts " "
+      puts "That's Bingo! You Win! "
+
+      return @win
+    end
+
+  end
+
+  def give_feedback_based_on_guess(guess_to_color_balls, color_code_to_color_balls)
+    @feedback = []
+
+    guess_to_color_balls.each_with_index do |guess, index_of_guess|
+      if guess == color_code_to_color_balls[index_of_guess]
+        @feedback << '🔴'
+      elsif color_code_to_color_balls.any?(guess)  
+        @feedback << '⚪'
+        
+      end
+    end
+
+    return @feedback 
+  end
 
 
 
--- main.rb
+++ main.rb
@@ -1,120 +1,22 @@
+require_relative 'lib/game'
+
 colors = {
-    1 => "🔴",
-    2 => "🔵",
-    3 => "🟢",
-    4 => "🟡",
-    5 => "🟣",
-    6 => "🟠", 
-    "1" => "🔴",
-    "2" => "🔵",
-    "3" => "🟢",
-    "4" => "🟡",
-    "5" => "🟣",
-    "6" => "🟠"
-  }
+  1 => "🔴",
+  2 => "🔵",
+  3 => "🟢",
+  4 => "🟡",
+  5 => "🟣",
+  6 => "🟠", 
+  "1" => "🔴",
+  "2" => "🔵",
+  "3" => "🟢",
+  "4" => "🟡",
+  "5" => "🟣",
+  "6" => "🟠"
+}
 
 attempts = 12
-  
-def start_game(colors, attempts)
-  color_code = computer_create_code()
-  # puts "Here is the created color code: "
-  color_code_to_color_balls = convert_array_to_color_balls(color_code, colors)
 
-  attempts.times do
-    puts "Attempt ##{attempts}" 
-    print "Please enter your guess: "
-    guess = gets.chomp
-    attempts -= 1
+game = Game.new(colors, attempts)
 
-    guess_to_array = guess.split("")
-
-    guess_to_color_balls = convert_array_to_color_balls(guess_to_array, colors)
-    
-    puts "Your guess is: #{guess_to_color_balls}" 
-
-    puts " "
-
-    win = check_if_guess_matches_with_code(guess_to_color_balls, color_code_to_color_balls)
-
-    if win == true 
-      break
-    end
-
-
-    feedback = give_feedback_based_on_guess(guess_to_color_balls, color_code_to_color_balls)
-    puts "Feedback: #{feedback}"
-
-  end
-
-  puts "The color code is: #{color_code_to_color_balls}"  
-
-
-end
-
-
-def give_feedback_based_on_guess(guess_to_color_balls, color_code_to_color_balls)
-  feedback = []
-
-  guess_to_color_balls.each_with_index do |guess, index_of_guess|
-    if guess == color_code_to_color_balls[index_of_guess]
-      feedback << '🔴'
-    elsif color_code_to_color_balls.any?(guess)  
-      feedback << '⚪'
-      
-    end
-  end
-
-  # guess_to_color_balls.each_with_index do |guess, index_of_guess|
-  #   puts guess 
-  #   if color_code_to_color_balls.any?(guess)  
-  #     feedback << "⚪" 
-  #   end
-  # end
-
-  return feedback 
-end
-
-
-def check_if_guess_matches_with_code(guess_to_color_code, color_code_to_color_balls)
-  # p guess_to_color_code
-  
-  win = true
-
-  if guess_to_color_code == color_code_to_color_balls
-    puts " "
-    puts "That's Bingo! You Win! "
-
-    return win
-  end
-
-end
-
-  
-def computer_create_code()
-  color_code = []
-  4.times do 
-    color_code.push(rand(1..6))
-    
-  end
-  return color_code
-end
-
-def convert_array_to_color_balls(color_code, colors)
-  converted_color = []
-  
-  color_code.each do |color_number|
-    converted_color.push(colors[color_number])
-  end
-
-  return converted_color
-end
-
-def display_introduction
-  puts "--- Mastermind Minigame ---"
-  puts ""
-end
-
-
-
-start_game(colors, attempts)
-
+game.start_game
